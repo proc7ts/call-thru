071e1c1223eb25cdaeb5b7110e80ddb5e8a194cf
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2018-12-02 15:09:13 +0700

    Improve the `callThru()` signature
    Pass parameters are inferred correctly now

--- src/call-thru.ts
+++ src/call-thru.ts
@@ -1,130 +1,205 @@
 import { NextCall } from './next-call';
 import { PassedThru } from './passed-thru';
+import Args = NextCall.Callee.Args;
 import Last = NextCall.LastOutcome;
 import Out = NextCall.Outcome;
 
-export type CallPass<I, O> = (this: void, ...args: NextCall.Callee.Args<I>) => O;
+export function callThru<P1 extends any[], R>(
+    fn: (this: void, ...args: P1) => R):
+    (this: void, ...args: P1) => Last<R>;
 
-export function callThru<P extends any[], R>(
-    fn: (this: void, ...args: P) => R):
-    (this: void, ...args: P) => Last<R>;
+export function callThru<
+    P1 extends any[], R1,
+    P2 extends Args<R1>, R2>(
+    fn1: (this: void, ...args: P1) => R1,
+    fn2: (this: void, ...args: P2) => R2):
+    (this: void, ...args: P1) => Out<R1, Last<R2>>;
 
-export function callThru<P extends any[], R1, R2>(
-    fn1: (this: void, ...args: P) => R1,
-    fn2: CallPass<R1, R2>):
-    (this: void, ...args: P) => Out<R1, Last<R2>>;
+export function callThru<
+    P1 extends any[], R1,
+    P2 extends Args<R1>, R2,
+    P3 extends Args<R2>, R3>(
+    fn1: (this: void, ...args: P1) => R1,
+    fn2: (this: void, ...args: P2) => R2,
+    fn3: (this: void, ...args: P3) => R3):
+    (this: void, ...args: P1) => Out<R1, Out<R2, Last<R3>>>;
 
-export function callThru<P extends any[], R1, R2, R3>(
-    fn1: (this: void, ...args: P) => R1,
-    fn2: CallPass<R1, R2>,
-    fn3: CallPass<R2, R3>):
-    (this: void, ...args: P) => Out<R1, Out<R2, Last<R3>>>;
+export function callThru<
+    P1 extends any[], R1,
+    P2 extends Args<R1>, R2,
+    P3 extends Args<R2>, R3,
+    P4 extends Args<R3>, R4>(
+    fn1: (this: void, ...args: P1) => R1,
+    fn2: (this: void, ...args: P2) => R2,
+    fn3: (this: void, ...args: P3) => R3,
+    fn4: (this: void, ...args: P4) => R4):
+    (this: void, ...args: P1) => Out<R1, Out<R2, Out<R3, Last<R4>>>>;
 
-export function callThru<P extends any[], R1, R2, R3, R4>(
-    fn1: (this: void, ...args: P) => R1,
-    fn2: CallPass<R1, R2>,
-    fn3: CallPass<R2, R3>,
-    fn4: CallPass<R3, R4>):
-    (this: void, ...args: P) => Out<R1, Out<R2, Out<R3, Last<R4>>>>;
+export function callThru<
+    P1 extends any[], R1,
+    P2 extends Args<R1>, R2,
+    P3 extends Args<R2>, R3,
+    P4 extends Args<R3>, R4,
+    P5 extends Args<R4>, R5>(
+    fn1: (this: void, ...args: P1) => R1,
+    fn2: (this: void, ...args: P2) => R2,
+    fn3: (this: void, ...args: P3) => R3,
+    fn4: (this: void, ...args: P4) => R4,
+    fn5: (this: void, ...args: P5) => R5):
+    (this: void, ...args: P1) => Out<R1, Out<R2, Out<R3, Out<R4, Last<R5>>>>>;
 
-export function callThru<P extends any[], R1, R2, R3, R4, R5>(
-    fn1: (this: void, ...args: P) => R1,
-    fn2: CallPass<R1, R2>,
-    fn3: CallPass<R2, R3>,
-    fn4: CallPass<R3, R4>,
-    fn5: CallPass<R4, R5>):
-    (this: void, ...args: P) => Out<R1, Out<R2, Out<R3, Out<R4, Last<R5>>>>>;
-
-export function callThru<P extends any[], R1, R2, R3, R4, R5, R6>(
-    fn1: (this: void, ...args: P) => R1,
-    fn2: CallPass<R1, R2>,
-    fn3: CallPass<R2, R3>,
-    fn4: CallPass<R3, R4>,
-    fn5: CallPass<R4, R5>,
-    fn6: CallPass<R5, R6>):
-    (this: void, ...args: P) => Out<R1, Out<R2, Out<R3, Out<R4, Out<R5,
+export function callThru<
+    P1 extends any[], R1,
+    P2 extends Args<R1>, R2,
+    P3 extends Args<R2>, R3,
+    P4 extends Args<R3>, R4,
+    P5 extends Args<R4>, R5,
+    P6 extends Args<R5>, R6>(
+    fn1: (this: void, ...args: P1) => R1,
+    fn2: (this: void, ...args: P2) => R2,
+    fn3: (this: void, ...args: P3) => R3,
+    fn4: (this: void, ...args: P4) => R4,
+    fn5: (this: void, ...args: P5) => R5,
+    fn6: (this: void, ...args: P6) => R6):
+    (this: void, ...args: P1) => Out<R1, Out<R2, Out<R3, Out<R4, Out<R5,
         Last<R6>>>>>>;
 
-export function callThru<P extends any[], R1, R2, R3, R4, R5, R6, R7>(
-    fn1: (this: void, ...args: P) => R1,
-    fn2: CallPass<R1, R2>,
-    fn3: CallPass<R2, R3>,
-    fn4: CallPass<R3, R4>,
-    fn5: CallPass<R4, R5>,
-    fn6: CallPass<R5, R6>,
-    fn7: CallPass<R6, R7>):
-    (this: void, ...args: P) => Out<R1, Out<R2, Out<R3, Out<R4, Out<R5,
+export function callThru<
+    P1 extends any[], R1,
+    P2 extends Args<R1>, R2,
+    P3 extends Args<R2>, R3,
+    P4 extends Args<R3>, R4,
+    P5 extends Args<R4>, R5,
+    P6 extends Args<R5>, R6,
+    P7 extends Args<R6>, R7>(
+    fn1: (this: void, ...args: P1) => R1,
+    fn2: (this: void, ...args: P2) => R2,
+    fn3: (this: void, ...args: P3) => R3,
+    fn4: (this: void, ...args: P4) => R4,
+    fn5: (this: void, ...args: P5) => R5,
+    fn6: (this: void, ...args: P6) => R6,
+    fn7: (this: void, ...args: P7) => R7):
+    (this: void, ...args: P1) => Out<R1, Out<R2, Out<R3, Out<R4, Out<R5,
         Out<R6, Last<R7>>>>>>>;
 
-export function callThru<P extends any[], R1, R2, R3, R4, R5, R6, R7, R8>(
-    fn1: (this: void, ...args: P) => R1,
-    fn2: CallPass<R1, R2>,
-    fn3: CallPass<R2, R3>,
-    fn4: CallPass<R3, R4>,
-    fn5: CallPass<R4, R5>,
-    fn6: CallPass<R5, R6>,
-    fn7: CallPass<R6, R7>,
-    fn8: CallPass<R7, R8>):
-    (this: void, ...args: P) => Out<R1, Out<R2, Out<R3, Out<R4, Out<R5,
+export function callThru<
+    P1 extends any[], R1,
+    P2 extends Args<R1>, R2,
+    P3 extends Args<R2>, R3,
+    P4 extends Args<R3>, R4,
+    P5 extends Args<R4>, R5,
+    P6 extends Args<R5>, R6,
+    P7 extends Args<R6>, R7,
+    P8 extends Args<R7>, R8>(
+    fn1: (this: void, ...args: P1) => R1,
+    fn2: (this: void, ...args: P2) => R2,
+    fn3: (this: void, ...args: P3) => R3,
+    fn4: (this: void, ...args: P4) => R4,
+    fn5: (this: void, ...args: P5) => R5,
+    fn6: (this: void, ...args: P6) => R6,
+    fn7: (this: void, ...args: P7) => R7,
+    fn8: (this: void, ...args: P8) => R8):
+    (this: void, ...args: P1) => Out<R1, Out<R2, Out<R3, Out<R4, Out<R5,
         Out<R6, Out<R7, Last<R8>>>>>>>>;
 
-export function callThru<P extends any[], R1, R2, R3, R4, R5, R6, R7, R8, R9>(
-    fn1: (this: void, ...args: P) => R1,
-    fn2: CallPass<R1, R2>,
-    fn3: CallPass<R2, R3>,
-    fn4: CallPass<R3, R4>,
-    fn5: CallPass<R4, R5>,
-    fn6: CallPass<R5, R6>,
-    fn7: CallPass<R6, R7>,
-    fn8: CallPass<R7, R8>,
-    fn9: CallPass<R8, R9>):
-    (this: void, ...args: P) => Out<R1, Out<R2, Out<R3, Out<R4, Out<R5, Out<R6, Out<R7, Out<R8, Last<R9>>>>>>>>>;
+export function callThru<
+    P1 extends any[], R1,
+    P2 extends Args<R1>, R2,
+    P3 extends Args<R2>, R3,
+    P4 extends Args<R3>, R4,
+    P5 extends Args<R4>, R5,
+    P6 extends Args<R5>, R6,
+    P7 extends Args<R6>, R7,
+    P8 extends Args<R7>, R8,
+    P9 extends Args<R8>, R9>(
+    fn1: (this: void, ...args: P1) => R1,
+    fn2: (this: void, ...args: P2) => R2,
+    fn3: (this: void, ...args: P3) => R3,
+    fn4: (this: void, ...args: P4) => R4,
+    fn5: (this: void, ...args: P5) => R5,
+    fn6: (this: void, ...args: P6) => R6,
+    fn7: (this: void, ...args: P7) => R7,
+    fn8: (this: void, ...args: P8) => R8,
+    fn9: (this: void, ...args: P9) => R9):
+    (this: void, ...args: P1) => Out<R1, Out<R2, Out<R3, Out<R4, Out<R5, Out<R6, Out<R7, Out<R8, Last<R9>>>>>>>>>;
 
-export function callThru<P extends any[], R1, R2, R3, R4, R5, R6, R7, R8, R9, R10>(
-    fn1: (this: void, ...args: P) => R1,
-    fn2: CallPass<R1, R2>,
-    fn3: CallPass<R2, R3>,
-    fn4: CallPass<R3, R4>,
-    fn5: CallPass<R4, R5>,
-    fn6: CallPass<R5, R6>,
-    fn7: CallPass<R6, R7>,
-    fn8: CallPass<R7, R8>,
-    fn9: CallPass<R8, R9>,
-    fn10: CallPass<R9, R10>):
-    (this: void, ...args: P) => Out<R1, Out<R2, Out<R3, Out<R4, Out<R5,
+export function callThru<
+    P1 extends any[], R1,
+    P2 extends Args<R1>, R2,
+    P3 extends Args<R2>, R3,
+    P4 extends Args<R3>, R4,
+    P5 extends Args<R4>, R5,
+    P6 extends Args<R5>, R6,
+    P7 extends Args<R6>, R7,
+    P8 extends Args<R7>, R8,
+    P9 extends Args<R8>, R9,
+    P10 extends Args<R9>, R10>(
+    fn1: (this: void, ...args: P1) => R1,
+    fn2: (this: void, ...args: P2) => R2,
+    fn3: (this: void, ...args: P3) => R3,
+    fn4: (this: void, ...args: P4) => R4,
+    fn5: (this: void, ...args: P5) => R5,
+    fn6: (this: void, ...args: P6) => R6,
+    fn7: (this: void, ...args: P7) => R7,
+    fn8: (this: void, ...args: P8) => R8,
+    fn9: (this: void, ...args: P9) => R9,
+    fn10: (this: void, ...args: P10) => R10):
+    (this: void, ...args: P1) => Out<R1, Out<R2, Out<R3, Out<R4, Out<R5,
         Out<R6, Out<R7, Out<R8, Out<R9, Last<R10>>>>>>>>>>;
 
-export function callThru<P extends any[], R1, R2, R3, R4, R5,
-    R6, R7, R8, R9, R10, R11>(
-    fn1: (this: void, ...args: P) => R1,
-    fn2: CallPass<R1, R2>,
-    fn3: CallPass<R2, R3>,
-    fn4: CallPass<R3, R4>,
-    fn5: CallPass<R4, R5>,
-    fn6: CallPass<R5, R6>,
-    fn7: CallPass<R6, R7>,
-    fn8: CallPass<R7, R8>,
-    fn9: CallPass<R8, R9>,
-    fn10: CallPass<R9, R10>,
-    fn11: CallPass<R10, R11>):
-    (this: void, ...args: P) => Out<R1, Out<R2, Out<R3, Out<R4, Out<R5,
+export function callThru<
+    P1 extends any[], R1,
+    P2 extends Args<R1>, R2,
+    P3 extends Args<R2>, R3,
+    P4 extends Args<R3>, R4,
+    P5 extends Args<R4>, R5,
+    P6 extends Args<R5>, R6,
+    P7 extends Args<R6>, R7,
+    P8 extends Args<R7>, R8,
+    P9 extends Args<R8>, R9,
+    P10 extends Args<R9>, R10,
+    P11 extends Args<R10>, R11>(
+    fn1: (this: void, ...args: P1) => R1,
+    fn2: (this: void, ...args: P2) => R2,
+    fn3: (this: void, ...args: P3) => R3,
+    fn4: (this: void, ...args: P4) => R4,
+    fn5: (this: void, ...args: P5) => R5,
+    fn6: (this: void, ...args: P6) => R6,
+    fn7: (this: void, ...args: P7) => R7,
+    fn8: (this: void, ...args: P8) => R8,
+    fn9: (this: void, ...args: P9) => R9,
+    fn10: (this: void, ...args: P10) => R10,
+    fn11: (this: void, ...args: P11) => R11):
+    (this: void, ...args: P1) => Out<R1, Out<R2, Out<R3, Out<R4, Out<R5,
         Out<R6, Out<R7, Out<R8, Out<R9, Out<R10,
             Last<R11>>>>>>>>>>>;
 
-export function callThru<P extends any[], R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12>(
-    fn1: (this: void, ...args: P) => R1,
-    fn2: CallPass<R1, R2>,
-    fn3: CallPass<R2, R3>,
-    fn4: CallPass<R3, R4>,
-    fn5: CallPass<R4, R5>,
-    fn6: CallPass<R5, R6>,
-    fn7: CallPass<R6, R7>,
-    fn8: CallPass<R7, R8>,
-    fn9: CallPass<R8, R9>,
-    fn10: CallPass<R9, R10>,
-    fn11: CallPass<R10, R11>,
-    fn12: CallPass<R11, R12>):
-    (this: void, ...args: P) => Out<R1, Out<R2, Out<R3, Out<R4, Out<R5,
+export function callThru<
+    P1 extends any[], R1,
+    P2 extends Args<R1>, R2,
+    P3 extends Args<R2>, R3,
+    P4 extends Args<R3>, R4,
+    P5 extends Args<R4>, R5,
+    P6 extends Args<R5>, R6,
+    P7 extends Args<R6>, R7,
+    P8 extends Args<R7>, R8,
+    P9 extends Args<R8>, R9,
+    P10 extends Args<R9>, R10,
+    P11 extends Args<R10>, R11,
+    P12 extends Args<R11>, R12>(
+    fn1: (this: void, ...args: P1) => R1,
+    fn2: (this: void, ...args: P2) => R2,
+    fn3: (this: void, ...args: P3) => R3,
+    fn4: (this: void, ...args: P4) => R4,
+    fn5: (this: void, ...args: P5) => R5,
+    fn6: (this: void, ...args: P6) => R6,
+    fn7: (this: void, ...args: P7) => R7,
+    fn8: (this: void, ...args: P8) => R8,
+    fn9: (this: void, ...args: P9) => R9,
+    fn10: (this: void, ...args: P10) => R10,
+    fn11: (this: void, ...args: P11) => R11,
+    fn12: (this: void, ...args: P12) => R12):
+    (this: void, ...args: P1) => Out<R1, Out<R2, Out<R3, Out<R4, Out<R5,
         Out<R6, Out<R7, Out<R8, Out<R9, Out<R10,
             Out<R11, Last<R12>>>>>>>>>>>>;
 
@@ -142,21 +217,34 @@ export function callThru<P extends any[], R1, R2, R3, R4, R5, R6, R7, R8, R9, R1
  *
  * A `NextCall` instance returned the pass is responsible for next function call and may modify the call outcome.
  */
-export function callThru<P extends any[], R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13>(
-    fn1: (this: void, ...args: P) => R1,
-    fn2: CallPass<R1, R2>,
-    fn3: CallPass<R2, R3>,
-    fn4: CallPass<R3, R4>,
-    fn5: CallPass<R4, R5>,
-    fn6: CallPass<R5, R6>,
-    fn7: CallPass<R6, R7>,
-    fn8: CallPass<R7, R8>,
-    fn9: CallPass<R8, R9>,
-    fn10: CallPass<R9, R10>,
-    fn11: CallPass<R10, R11>,
-    fn12: CallPass<R11, R12>,
-    fn13: CallPass<R12, R13>):
-    (this: void, ...args: P) => Out<R1, Out<R2, Out<R3, Out<R4, Out<R5,
+export function callThru<
+    P1 extends any[], R1,
+    P2 extends Args<R1>, R2,
+    P3 extends Args<R2>, R3,
+    P4 extends Args<R3>, R4,
+    P5 extends Args<R4>, R5,
+    P6 extends Args<R5>, R6,
+    P7 extends Args<R6>, R7,
+    P8 extends Args<R7>, R8,
+    P9 extends Args<R8>, R9,
+    P10 extends Args<R9>, R10,
+    P11 extends Args<R10>, R11,
+    P12 extends Args<R11>, R12,
+    P13 extends Args<R12>, R13>(
+    fn1: (this: void, ...args: P1) => R1,
+    fn2: (this: void, ...args: P2) => R2,
+    fn3: (this: void, ...args: P3) => R3,
+    fn4: (this: void, ...args: P4) => R4,
+    fn5: (this: void, ...args: P5) => R5,
+    fn6: (this: void, ...args: P6) => R6,
+    fn7: (this: void, ...args: P7) => R7,
+    fn8: (this: void, ...args: P8) => R8,
+    fn9: (this: void, ...args: P9) => R9,
+    fn10: (this: void, ...args: P10) => R10,
+    fn11: (this: void, ...args: P11) => R11,
+    fn12: (this: void, ...args: P12) => R12,
+    fn13: (this: void, ...args: P13) => R13):
+    (this: void, ...args: P1) => Out<R1, Out<R2, Out<R3, Out<R4, Out<R5,
         Out<R6, Out<R7, Out<R8, Out<R9, Out<R10,
             Out<R11, Out<R12, Last<R13>>>>>>>>>>>>>;
 
