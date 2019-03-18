15a9bdbba2593d50ae2d870148b157fefb8494b0
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2019-03-18 10:18:11 +0700

    Build CommonJS instead of UMD

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   collectCoverage: true,
   collectCoverageFrom: [
-    'src/**/*.{ts}',
+    'src/**/*.ts',
     '!src/**/*.spec.ts',
     '!src/**/index.ts',
     '!**/node_modules/**',
