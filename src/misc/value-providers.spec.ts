b2c86020efeb7e75b3c80c7948938dd54398ef3f
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-04-11 16:25:39 +0700

    Add `lazyValue()` utility function

--- src/misc/value-providers.spec.ts
+++ src/misc/value-providers.spec.ts
@@ -1,8 +1,33 @@
-import { valueProvider, valuesCloner, valuesProvider } from './value-providers';
+import { lazyValue, valueProvider, valuesCloner, valuesProvider } from './value-providers';
 
 describe('valueProvider', () => {
   it('always returns its argument', () => {
     expect(valueProvider('abc')()).toBe('abc');
+  });
+});
+
+describe('lazyValue', () => {
+
+  let provider: jest.Mock<string>;
+  let lazy: () => string;
+
+  beforeEach(() => {
+    provider = jest.fn(() => 'test');
+    lazy = lazyValue(provider);
+  });
+
+  it('does not call provider until requested', () => {
+    expect(provider).not.toHaveBeenCalled();
+  });
+  it('evaluates the value when called', () => {
+    expect(lazy()).toBe('test');
+    expect(provider).toHaveBeenCalledTimes(1);
+  });
+  it('does not re-evaluate the value', () => {
+    lazy();
+    provider.mockImplementation(() => 'other');
+    expect(lazy()).toBe('test');
+    expect(provider).toHaveBeenCalledTimes(1);
   });
 });
 
--- src/misc/value-providers.ts
+++ src/misc/value-providers.ts
@@ -14,6 +14,22 @@ export function valueProvider<T>(value: T): (this: void) => T {
 }
 
 /**
+ * Creates a provider of lazily evaluated value.
+ *
+ * The returned function evaluates the value first time it is called. Then it just returns previously evaluated value.
+ *
+ * @param provider  A no-arg function evaluating the value.
+ *
+ * @returns A function that returns the value evaluated by `provider`.
+ */
+export function lazyValue<T>(provider: (this: void) => T): (this: void) => T {
+
+  let get = (): T => (get = valueProvider(provider()))();
+
+  return () => get();
+}
+
+/**
  * Creates a provider of arguments tuple.
  *
  * The returned tuple should never be modified as it is not cloned.
